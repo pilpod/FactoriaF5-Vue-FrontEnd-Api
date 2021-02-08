386230c60809b3d1e2d578a22211b243d00cdc04
pilpod <67429595+pilpod@users.noreply.github.com>
2021-02-08 20:06:47 +0100

    axios installed

--- src/components/Dish.vue
+++ src/components/Dish.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="dish">
         <h1>Productos</h1>
-        <div class="product-img">
-            <img v-bind:src="product.img" alt="" />
+        <div v-for="dish in products" :key="dish.id" class="product">
+            <div class="product-img">
+                <img v-bind:src="dish.img" alt="" />
+            </div>
+            <div>
+                <h1>{{ dish.name }}</h1>
+                <span>{{ dish.ingredients }}</span>
+            </div>
         </div>
-        <h1>{{ product.name }}</h1>
-        <p>{{ product.ingredients }}</p>
     </div>
 </template>
 
@@ -19,23 +23,47 @@ export default {
     },
     data() {
         return {
-            product: {
-                img: 'http://placeimg.com/640/480/any/grayscale',
-                name: 'Caracoles en salsa',
-                ingredients: 'lo que más guste',
-            },
+            products: [
+                {
+                    id: 1,
+                    img: 'http://placeimg.com/640/480/any/grayscale',
+                    name: 'Caracoles en salsa',
+                    ingredients: 'lo que más guste',
+                },
+                {
+                    id: 2,
+                    img: 'http://placeimg.com/640/480/any/grayscale',
+                    name: 'Ternera en salsa',
+                    ingredients: 'lo que más guste',
+                },
+            ],
         }
     },
 }
 </script>
 
 <style scoped>
+.product {
+    border: solid 1px gray;
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    padding: 10px;
+}
+
+.product h1 {
+    margin: 0;
+    padding: 0;
+}
+
 .product-img > img {
-    width: 200px;
-    height: 200px;
+    width: 80px;
+    height: 80px;
     border: 2px solid blueviolet;
     border-radius: 100px;
     object-fit: cover;
     object-position: 50% 50%;
+    margin: 0 20px 0 0;
 }
 </style>
